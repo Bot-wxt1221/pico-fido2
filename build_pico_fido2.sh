ecb8ab47d9827b77b5c4699fee46e3f0576f7bcb
wxt <3264117476@qq.com>
2026-03-28 23:06:04 +0800

    Update

--- build_pico_fido2.sh
+++ build_pico_fido2.sh
@@ -20,7 +20,7 @@ for board in "waveshare_rp2350_one.h"
 do
     board_name="$(basename -- "$board" .h)"
     rm -rf -- ./*
-    PICO_SDK_PATH="${PICO_SDK_PATH}" cmake .. -DPICO_BOARD=$board_name -DENABLE_PQC=1 -DSECURE_BOOT_PKEY=${SECURE_BOOT_PKEY} -DENABLE_EDDSA=1 -DVIDPID=Yubikey5 -DPICO_DEFAULT_WS2812_PIN=16
+    PICO_SDK_PATH="${PICO_SDK_PATH}" cmake .. -DPICO_BOARD=$board_name -DSECURE_BOOT_PKEY=${SECURE_BOOT_PKEY} -DENABLE_EDDSA=1 -DVIDPID=Yubikey5 -DPICO_DEFAULT_WS2812_PIN=16
     make -j`nproc`
     mv pico_fido2.uf2 ../release/pico_fido2_$board_name-$SUFFIX.uf2
     mv ../pico-keys-sdk/otp.json ../release/pico_fido2_$board_name-$SUFFIX-otp.json
